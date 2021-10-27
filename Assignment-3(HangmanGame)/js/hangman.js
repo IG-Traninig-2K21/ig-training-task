f58599bafb4b44fa115275f97d5b7f793ae10a6b
Anshika Gupta <67957243+Anshika1301@users.noreply.github.com>
2021-10-27 17:11:43 +0530

    add files

--- Assignment-3(HangmanGame)/js/hangman.js
+++ Assignment-3(HangmanGame)/js/hangman.js
@@ -35,7 +35,7 @@ function generateButtons() {
   let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
     `
       <button
-        class="btn btn-lg btn-primary m-2"
+        class="btn btn-lg btn-light m-2"
         id='` + letter + `'
         onClick="handleGuess('` + letter + `')"
       >
