4a1bf571194525329d5ae48685684deca7ba60ea
Anshika Gupta <67957243+Anshika1301@users.noreply.github.com>
2021-11-12 16:59:57 +0530

    add files

--- Canvas_MS_Paint/js/script.js
+++ Canvas_MS_Paint/js/script.js
@@ -4,9 +4,12 @@ var canvas = document.getElementById("mycanvas");
 canvas.width = window.innerWidth;     //it gives viewport width
 canvas.height = 500;
 var shapes = canvas.getContext("2d");  
+var currColor; 
+
 /*---------- more responsive code start here ------------*/
 
-function drawShapes()
+
+function drawShapes(flag)
 {
     $("#mycanvas").mousedown(function(event)
     {
@@ -15,14 +18,138 @@ function drawShapes()
             shapes.lineTo(event.pageX, event.pageY);    // return co-ordinate according to mouse
             console.log(event.pageX, event.pageY);
             shapes.stroke();
+
+            if(flag=="false")
+            {
+                shapes.strokeStyle="white";
+            }
         })
 
         $("#mycanvas").mouseup(function(event)
         {
-            $("#mycanvas").unbind("mousedown");
+            $("#mycanvas").unbind("mousemove");     //remove mousedown event
+            flag='true';
         })
     })
 }
 
+function colorChange(color)
+{
+    currColor = color.value;
+    shapes.strokeStyle= currColor;
+}
+
+function sliderCall(penSize)
+{
+    shapes.lineWidth = penSize.value;
+}
+
+function clearScreen()
+{
+    shapes.fillStyle = "white";
+    shapes.fillRect(0,0,window.innerWidth, 500);
+}
+
+
+var Rectangle = (function () {
+    function Rectangle(canvas) {
+        var inst=this;
+        this.canvas = canvas;
+        this.className= 'Rectangle';
+        this.isDrawing = false;
+        this.bindEvents();
+    }
+
+	 Rectangle.prototype.bindEvents = function() {
+    var inst = this;
+    inst.canvas.on('mouse:down', function(o) {
+      inst.onMouseDown(o);
+    });
+    inst.canvas.on('mouse:move', function(o) {
+      inst.onMouseMove(o);
+    });
+    inst.canvas.on('mouse:up', function(o) {
+      inst.onMouseUp(o);
+    });
+    inst.canvas.on('object:moving', function(o) {
+      inst.disable();
+    })
+  }
+    Rectangle.prototype.onMouseUp = function (o) {
+      var inst = this;
+      inst.disable();
+    };
+
+    Rectangle.prototype.onMouseMove = function (o) {
+      var inst = this;
+      
+
+      if(!inst.isEnable()){ return; }
+      console.log("mouse move rectange");
+      var pointer = inst.canvas.getPointer(o.e);
+      var activeObj = inst.canvas.getActiveObject();
+
+      activeObj.stroke= 'red',
+      activeObj.strokeWidth= 5;
+      activeObj.fill = 'transparent';
+
+      if(origX > pointer.x){
+          activeObj.set({ left: Math.abs(pointer.x) }); 
+      }
+      if(origY > pointer.y){
+          activeObj.set({ top: Math.abs(pointer.y) });
+      }
+
+      activeObj.set({ width: Math.abs(origX - pointer.x) });
+      activeObj.set({ height: Math.abs(origY - pointer.y) });
+
+      activeObj.setCoords();
+      inst.canvas.renderAll();
+
+    };
+
+    Rectangle.prototype.onMouseDown = function (o) {
+      var inst = this;
+      inst.enable();
+
+      var pointer = inst.canvas.getPointer(o.e);
+      origX = pointer.x;
+      origY = pointer.y;
+
+    	var rect = new fabric.Rect({
+          left: origX,
+          top: origY,
+          originX: 'left',
+          originY: 'top',
+          width: pointer.x-origX,
+          height: pointer.y-origY,
+          angle: 0,
+          transparentCorners: false,
+          hasBorders: false,
+          hasControls: false
+      });
+
+  	  inst.canvas.add(rect).setActiveObject(rect);
+    };
+
+    Rectangle.prototype.isEnable = function(){
+      return this.isDrawing;
+    }
+
+    Rectangle.prototype.enable = function(){
+      this.isDrawing = true;
+    }
+
+    Rectangle.prototype.disable = function(){
+      this.isDrawing = false;
+    }
+
+    return Rectangle;
+}());
+
+
+
+// var canvas = new fabric.Canvas('canvas');
+var arrow = new Rectangle(canvas);
 
 // window.onload = drawShapes;
